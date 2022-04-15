13a0f440869cdfcfe74372350276c5f9d931842a
Tenzin <tenthinten@gmail.com>
2022-04-15 12:58:27 +0530

    middle of array

---
+++ middleOfArray.js
@@ -0,0 +1,10 @@
+function middle_elements(a, b) 
+{
+  var new_array = []
+  new_array.push(a[1], b[1]);
+
+    return new_array;
+}
+console.log(middle_elements([1, 2, 3], [1, 5, 6]));  
+console.log(middle_elements([3, 3, 3], [2, 8, 0]));  
+console.log(middle_elements([4, 2, 7], [2, 4, 5])); 

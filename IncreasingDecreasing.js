3c1ebf0e227606ffb9e84d09deba5743977658a2
Tenzin <tenthinten@gmail.com>
2022-04-19 16:37:31 +0530

    either increasing or decreasing

---
+++ IncreasingDecreasing.js
@@ -0,0 +1,15 @@
+function is_monotonous(num) {
+    if (num.length === 1) {
+        return true;
+    }
+    var num_direction = num[1] - num[0];
+    for (var i = 0; i < num.length - 1; i++) {
+        if (num_direction * (num[i + 1] - num[i]) <= 0) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(is_monotonous([1, 2, 3]));
+console.log(is_monotonous([1, 2, 2]))
+console.log(is_monotonous([-3, -2, -1]))
